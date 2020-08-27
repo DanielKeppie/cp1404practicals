006e5c5bb1b9d920df947d2afb8fd84c8b0bebce
Daniel Keppie <daniel.keppie@my.jcu.edu.au>
2020-08-27 15:56:05 +1000

    Refactor broken_score to have functions

--- cp1404practicals/prac_03/broken_score.py
+++ cp1404practicals/prac_03/broken_score.py
@@ -5,13 +5,27 @@ Broken program to determine score status
 
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100: # Changed to while loop, added or > 100.
-    print("Invalid score")
-    score = float(input("Enter score: ")) # Added an escape to the while loop
-if score > 90: # Ordered them properly
-    print("Your grade is excellent!")
-elif score > 50:
-    print("Your grade is passable")
-else:
-    print("You have failed.")
+def main():
+    score = get_valid_score()
+    grade = calculate_result(score)
+    print("With our score of {} you have received a {} ".format(score, grade))
+
+
+def calculate_result(score):
+    if score > 90:  # Ordered them properly
+        print("excellent mark")
+    elif score > 50:
+        return("pass")
+    else:
+        return("fail")
+
+
+def get_valid_score():
+    score = int(input("What was your score? "))
+    while score < 0 or score > 100:  # Changed to while loop, added or > 100.
+        print("Invalid score")
+        score = float(input("Enter score: "))  # Added an escape to the while loop
+    return score
+
+
+main()
